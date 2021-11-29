b54cd2de8128d45775b22695d25f4928f168c09d
agnieszka.r.klimek <agnieszka.r.klimek@student.put.poznan.pl>
2021-11-29 13:44:54 +0100

    Image propositions added

--- LaneDetection/laneDetection.py
+++ LaneDetection/laneDetection.py
@@ -41,39 +41,41 @@ class LaneDetection(Image):
 
     def findLane(self):
         self.isolateLane()
-        self.laneFilter()
+        #self.laneFilter()
 
         self.image = feature.canny(self.image)
-        self.image = morphology.dilation(self.image, morphology.square(3))
+        self.show()
 
-        angles = np.linspace(-np.pi, np.pi, 360, endpoint=False)
-        h, theta, d = transform.hough_line(self.image, theta=angles)
-
-        for _, angle, dist in zip(*transform.hough_line_peaks(h, theta, d)):
-            (x0, y0) = dist * np.array([np.cos(angle), np.sin(angle)])
-            self.laneLines.append(
-                LinearFunction(
-                    np.tan(angle + np.pi / 2),
-                    y0 - np.tan(angle + np.pi / 2) * x0,
-                    x0, y0
-                )
-            )
-
-        h, w = self.image.shape
-
-        mini = min(self.laneLines, key= lambda x: x.a)
-        maxi = max(self.laneLines, key= lambda x: x.a)
-
-        plt.plot(
-            [0, mini.getArgument(h // 1.4)],
-            [mini.getValue(0), h // 1.4],
-            color='red', lw=10, alpha=0.5
-        )
-        plt.plot(
-            [maxi.getArgument(h // 1.4), w],
-            [h // 1.4, maxi.getValue(w)],
-            color='red', lw=10, alpha=0.5
-        )
-
-        plt.imshow(self.finalImage)
-        plt.show()
+        # self.image = morphology.dilation(self.image, morphology.square(3))
+        #
+        # angles = np.linspace(-np.pi, np.pi, 360, endpoint=False)
+        # h, theta, d = transform.hough_line(self.image, theta=angles)
+        #
+        # for _, angle, dist in zip(*transform.hough_line_peaks(h, theta, d)):
+        #     (x0, y0) = dist * np.array([np.cos(angle), np.sin(angle)])
+        #     self.laneLines.append(
+        #         LinearFunction(
+        #             np.tan(angle + np.pi / 2),
+        #             y0 - np.tan(angle + np.pi / 2) * x0,
+        #             x0, y0
+        #         )
+        #     )
+        #
+        # h, w = self.image.shape
+        #
+        # mini = min(self.laneLines, key= lambda x: x.a)
+        # maxi = max(self.laneLines, key= lambda x: x.a)
+        #
+        # plt.plot(
+        #     [0, mini.getArgument(h // 1.4)],
+        #     [mini.getValue(0), h // 1.4],
+        #     color='red', lw=10, alpha=0.5
+        # )
+        # plt.plot(
+        #     [maxi.getArgument(h // 1.4), w],
+        #     [h // 1.4, maxi.getValue(w)],
+        #     color='red', lw=10, alpha=0.5
+        # )
+        #
+        # plt.imshow(self.finalImage)
+        # plt.show()
