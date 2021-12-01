07f448220a78d36823b15e725620c8b5bf26cb03
aleksandra_1508 <ola.kiel@onet.pl>
2021-12-01 21:04:40 +0100

    upd image

--- Image/image.py
+++ Image/image.py
@@ -1,3 +1,4 @@
+import cv2
 from skimage import io, feature, transform, color, filters, draw, morphology, exposure
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,6 +8,7 @@ from typing import Union
 class Image(object):
     def __init__(self, path: str, as_gray: bool = True):
         self.image = io.imread(path, as_gray=as_gray)
+        self.imageOrg =cv2.imread(path)
         self.finalImage = io.imread(path, as_gray=False)
 
     def show(self):
