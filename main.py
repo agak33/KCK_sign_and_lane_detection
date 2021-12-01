20a62fa345ea9554a1f170475d99ffe2af48d631
agak33 <69504909+agak33@users.noreply.github.com>
2021-12-01 17:40:51 +0100

    main updated

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 from LaneDetection.laneDetection import LaneDetection
 
 if __name__ == '__main__':
-    # img = Image(f'img/road1.jpg')
-    # img.houghTransform()
-    for i in range(1, 15):
+    for i in range(66):
         img = LaneDetection(f'img/road{i}.jpg')
-        img.findLane()
-        #img.show()
-
-
+        img.findLane(f'results/road{i}.jpg')
