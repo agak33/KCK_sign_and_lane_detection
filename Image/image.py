26815afd80d1a17e2a2b1e7a8c0e148be8e0e046
aleksandra_1508 <ola.kiel@onet.pl>
2021-12-02 18:26:22 +0100

    upd image

--- Image/image.py
+++ Image/image.py
@@ -8,8 +8,9 @@ from typing import Union
 class Image(object):
     def __init__(self, path: str, as_gray: bool = True):
         self.image = io.imread(path, as_gray=as_gray)
-        self.imageOrg =cv2.imread(path)
+        self.imageOrg = cv2.imread(path)
         self.finalImage = io.imread(path, as_gray=False)
+        self.h, self.w = self.image.shape
 
     def show(self):
         plt.imshow(self.image)
