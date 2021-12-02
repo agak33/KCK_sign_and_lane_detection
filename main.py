fce83d83ccb30752d5607fbbf7143e9b614cdc14
aleksandra_1508 <ola.kiel@onet.pl>
2021-12-02 18:28:19 +0100

    upd main

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
 from LaneDetection.laneDetection import LaneDetection
+from signDetection.signDetection.signDetectionC import  Detect
+import matplotlib.pyplot as plt
+import cv2
 
 if __name__ == '__main__':
     for i in range(66):
-        img = LaneDetection(f'img/road{i}.jpg')
-        img.findLane(f'results/road{i}.jpg')
+        try:
+            img = LaneDetection(f'img/road{i}.jpg')
+            mini, maxi = img.findLane()
+            img1 = Detect(f'img/road{i}.jpg')
+            img1.detectSign(i)
+
+
+
+            plt.clf()
+
+            if mini is not None:
+                plt.plot(
+                    [0, mini.getArgument(img1.h * 0.85)],
+                    [mini.getValue(0), img1.h * 0.85],
+                    color='red', lw=10, alpha=0.5
+                )
+
+            if maxi is not None:
+                plt.plot(
+                    [maxi.getArgument(img1.h * 0.85), img1.w],
+                    [img1.h * 0.85, maxi.getValue(img1.w)],
+                    color='red', lw=10, alpha=0.5
+                )
+            plt.imshow(img1.finalImage)
+
+            plt.savefig(f'results/road{i}.jpg', bbox_inches='tight', dpi=300)
+            print(f"f'results/road{i}.jpg' saved")
+        except FileNotFoundError:
+            continue
+
+
+
+
